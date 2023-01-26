42729303f48e98bac5121ca839f75abdfe5152c4
Marina Silnova <marusia421@mail.ru>
2023-01-26 22:47:32 +0300

    Добавили домашнюю работу

--- third.py
+++ third.py
@@ -1,6 +1,6 @@
 #Задайте список случайных чисел от 1 до 10. Посчитайте, сколько всего совпадающих элементов есть в списке. Удалите все повторяющиеся элементы.
 import random
-random_list = [random.randint(1,11) for _ in range(15)]
+random_list = [random.randint(1,10) for _ in range(15)]
 print(f'изначальный список : {random_list}')
 count = {el for el in random_list if random_list.count(el)>1}
 print (f'повторяющиеся элементы : {count}')
